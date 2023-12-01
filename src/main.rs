5b05b83ce560edeaa5eade88178166a0b00daa4c
Cow <criacow@gmail.com>
2023-12-01 21:04:53 +0000

    put a proper html wrapper around the execution; deploy candidate

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,14 @@
 use lambda_http::{run, service_fn, Body, Error, Request, RequestExt, Response};
 use std::time::SystemTime;
 
-/// This is the main body for the function.
-/// Write your code inside it.
-/// There are some code example in the following URLs:
-/// - https://github.com/awslabs/aws-lambda-rust-runtime/tree/main/examples
+/// Main body. Calculate and return beats.
 async fn function_handler(event: Request) -> Result<Response<Body>, Error> {
-    // Extract some useful information from the request
     let time = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH);
     let seconds = time?.as_secs();
     let beats = (((seconds + 3600) % 86400) * 10) / 864;
 
-    let message = format!("The current time is: {:03}", beats);
+    let message = format!("<html><head><title>r u s t b e a t s  0.01</title></head><body><h1>The current time is: {:03}</h1></body></html>", beats);
 
-    // Return something that implements IntoResponse.
-    // It will be serialized to the right response event automatically by the runtime
     let resp = Response::builder()
         .status(200)
         .header("content-type", "text/html")
